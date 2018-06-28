52ade4a7d90b7d42b26ab856b2e39e36999485a6
Arthur <phenomich@gmail.com>
2018-06-28 16:20:24 +0300

    first

---
+++ binarySearch.js
@@ -0,0 +1,26 @@
+
+     function binarySearch( list,  item) {
+        var low = 0;
+        var high = list.length - 1;
+
+       while (low <= high) {
+            var mid =  Math.floor((high + low) / 2);
+           var  guess = list[mid];
+           if (guess == item) {
+               return mid;
+           }
+           if (guess > item) {
+               high = mid - 1;
+           } else {
+               low = mid + 1;
+           }
+       }
+
+       return null;
+   }
+
+ 
+       myList = [1, 3, 5, 7, 9];
+
+       console.log(binarySearch(myList, 5)); // 1
+       console.log(binarySearch(myList, -1)); // null
